dbe0c3bf3df1eb6978ce9ad235bc79e18d2380b0
Julien <julien.sebag@me.com>
2022-05-06 16:17:12 +0200

    Test fix API

--- api/index.js
+++ api/index.js
@@ -1,20 +1,6 @@
 const app = require("express")()
-const router = require("express").Router()
-const { v4 } = require("uuid")
 
-const auth = require("../routes/auth")
-router.use("/auth", auth)
-
-const users = require("../routes/users")
-router.use("/users", users)
-
-const restaurant = require("../routes/restaurant")
-router.use("/restaurant", restaurant)
-
-const delivery = require("../routes/delivery")
-router.use("/delivery", delivery)
-
-const recipes = require("../routes/recipes")
-router.use("/recipes", recipes)
+const allRoutes = require("../routes")
+app.use("/api", allRoutes)
 
 module.exports = app
