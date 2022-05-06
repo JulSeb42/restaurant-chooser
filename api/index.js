b3741165b731437ab4708fe8d2b65cea14319356
Julien <julien.sebag@me.com>
2022-05-06 16:58:21 +0200

    Re test fixes Vercel

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@
 require("dotenv/config")
 
 // ℹ️ Connects to the database
-require("./db")
+require("../db")
 
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
@@ -12,7 +12,7 @@ const express = require("express")
 const app = express()
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
-require("./config")(app)
+require("../config")(app)
 
 // 👇 Start handling routes here
 // Contrary to the views version, all routes are controlled from the routes/index.js
@@ -33,6 +33,6 @@ app.use((req, res) => {
 })
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require("./error-handling")(app)
+require("../error-handling")(app)
 
 module.exports = app
